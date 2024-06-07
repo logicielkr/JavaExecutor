c6459f6ac302fdd0588982c3a6e567e0ad812cfd
logicielkr <graha@graha.kr>
2024-06-07 10:09:02 +0900

    1. 버그 수정 및 기능개선
    - Ctrl + Enter 하면, 2번 실행된다 #8
    - 외부 CDN 을 1개로 통합 할 필요가 있다. #7

--- js/java.insert.js
+++ js/java.insert.js
@@ -250,7 +250,7 @@ function readyAutoSave(gbn) {
 			callAutoSave();
 		});
 		$("table#java td.contents textarea.contents").keypress(function (e) {
-			ctrlenter(e);
+//			ctrlenter(e);
 			callAutoSave();
 		});
 		$("table#java td.title input.title").keypress(function () {
